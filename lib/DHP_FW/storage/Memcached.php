04ed1b60fc2278ca7612fd529bd7025be1fe6f46
Henrik Pejer <mrpejer@gmail.com>
2013-02-03 19:22:50 +0100

    Added stubs for storage

---
+++ lib/DHP_FW/storage/Memcached.php
@@ -0,0 +1,10 @@
+<?php
+declare(encoding = "UTF8") ;
+namespace DHP_FW\storage;
+/**
+ * User: Henrik Pejer mr@henrikpejer.com
+ * Date: 2013-02-03 19:21
+ */
+class Memcached implements StorageInterface{
+
+}
---
+++ lib/DHP_FW/storage/StorageInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(encoding="UTF8");
+namespace DHP_FW\storage;
+
+/**
+ * User: Henrik Pejer mr@henrikpejer.com
+ * Date: 2013-02-03 19:21
+ */
+interface StorageInterface {
+
+}
