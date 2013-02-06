0c94b31382816fac5f5c1704d2992f8942510730
Henrik Pejer <mrpejer@gmail.com>
2013-02-06 18:43:23 +0100

    Added a first try for a storage interface.

--- lib/DHP_FW/storage/StorageInterface.php
+++ lib/DHP_FW/storage/StorageInterface.php
@@ -8,4 +8,58 @@ namespace DHP_FW\storage;
  */
 interface StorageInterface {
 
+    /**
+     * Opens storage
+     *
+     * @return bool
+     */
+    function open();
+
+    /**
+     * Saves data to storage
+     *
+     * @param mixed $data
+     *
+     * @return bool
+     */
+    function save($data);
+
+    /**
+     * Reads data from storage. Optionally with an offset and length
+     *
+     * @param int $offset
+     * @param int $length
+     *
+     * @return mixed
+     */
+    function read($offset = NULL, $length = NULL);
+
+    /**
+     * Truncates to zero
+     *
+     * @return bool
+     */
+    function truncate();
+
+    /**
+     * Removes the data and the storage
+     *
+     * @return bool
+     */
+    function delete();
+
+    /**
+     * Sets read-only: not possible to alter NOR save,truncate, delete or save the
+     * object
+     *
+     * @return bool
+     */
+    function setReadOnly();
+
+    /**
+     * Sets read-and-write : simply making it possible to update and save the object
+     *
+     * @return bool
+     */
+    function setReadAndWrite();
 }
