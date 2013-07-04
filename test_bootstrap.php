39dd18b907315e90362d00da94b054675ec63c3d
Henrik Pejer <mrpejer@gmail.com>
2013-07-04 11:03:15 +0200

    Updated to handle including files in a specific order

--- test_bootstrap.php
+++ test_bootstrap.php
@@ -2,6 +2,10 @@
 # require_once 'bootstrap.php';
 // lets automagicly try to include all lib/DHP files, ok?
 $autoIncludeRecursive = function ($root) {
+    if (file_exists($root) && is_file($root)){
+        require_once $root;
+        return true;
+    }
     $dh = opendir($root);
     while ($file = readdir($dh)) {
         if ($file{0} == '.') {
@@ -19,4 +23,5 @@ $autoIncludeRecursive('lib/DHP/Component');
 $autoIncludeRecursive('lib/DHP/middleware');
 $autoIncludeRecursive('lib/DHP/utility');
 $autoIncludeRecursive('lib/DHP/dependencyInjection');
+$autoIncludeRecursive('lib/DHP/EventInterface.php');
 $autoIncludeRecursive('lib/DHP');
