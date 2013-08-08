12431831792245bc00bdac91efb5ccbe941ca2da
Henrik Pejer <mrpejer@gmail.com>
2013-08-08 13:45:21 +0200

    Updated so that data posted to the script will have it's columns renamed prior to being sent to the database
    The reason for this is that since we rename the columns before they
    are sent to the browser, we should reverse the naming when we get
    data sent to us. Otherwise the mappings will be all wrong and there
    is a risk data will either not be saved or perhaps even corrupted.
    
    Also, cleaned and reformated code in the file...

--- lib/DHP/modules/propelJson/API.php
+++ lib/DHP/modules/propelJson/API.php
@@ -1,11 +1,12 @@
 <?php
-declare(encoding = "UTF8");
+declare( encoding = "UTF8" );
 /**
  * Created by Henrik Pejer ( mr@henrikpejer.com )
  * Date: 2013-07-18 20:11
  */
 
 namespace DHP\modules\propelJson;
+
 use DHP\Request;
 use DHP\Response;
 
@@ -15,6 +16,7 @@ use DHP\Response;
  * An attempt to make a smart API with the aid of Propel.
  *
  * We will try to implement most things from jsonapi.org : it looks good
+ *
  * @package DHP\modules
  */
 class API
@@ -68,7 +70,7 @@ class API
             # is this something for a model?
             if (in_array($uriParts[$i], array_keys($this->dataMap))) {
                 if ($i == $maxKey) {
-                    $this->dataCommands[$uriParts[$i]] = NULL;
+                    $this->dataCommands[$uriParts[$i]] = null;
                 } else {
                     $this->dataCommands[$uriParts[$i]] = $uriParts[++$i];
                 }
@@ -79,50 +81,70 @@ class API
     public function run()
     {
         $data            = array();
-        $previousCommand = NULL;
-        $gotResults = false;
+        $previousCommand = null;
+        $gotResults      = false;
         foreach ($this->dataCommands as $command => $value) {
-            $value = $value == '-'?NULL:$value;
-            if ( !isset($value) && isset($previousCommand) ){
+            $value = $value == '-' ? null : $value;
+            if (!isset( $value ) && isset( $previousCommand )) {
                 $value = $data[$previousCommand];
             }
-            try{
-                $dataApiObject   = new Data($this->propelNamespace . ucfirst($command), $value, $previousCommand);
-                $requestBody = $this->request->body;
-                if (!empty($requestBody)){
-                    $dataApiObject->setData($this->request->body);
+            try {
+                $dataApiObject = new Data( $this->propelNamespace . ucfirst($command), $value, $previousCommand );
+                $requestBody   = $this->request->body;
+                if (!empty( $requestBody )) {
+                    # this should reformat the request according to the map of
+                    $dataApiObject->setData($this->mapColumnsForInput($command,$requestBody));
                 }
-                $data[$command]  = $dataApiObject->getData();
+                $data[$command] = $dataApiObject->getData();
                 /** @noinspection PhpUndefinedMethodInspection */
-                if ($gotResults == false && $data[$command]->count() > 0){
+                if ($gotResults == false && $data[$command]->count() > 0) {
                     $gotResults = true;
                 }
                 $previousCommand = $command;
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 var_dump($e->getMessage());
             }
         }
-        if ( $gotResults == false){
+        if ($gotResults == false) {
             $this->response->setStatus(404);
         } else {
-            $this->response->addHeader('Content-Type','application/json');
+            $this->response->addHeader('Content-Type', 'application/json');
             $this->response->setContent($this->formatDataForResponse($data));
         }
     }
 
-    public function returnDataCommands()
+    /**
+     * This uses data sent by the client and renames the columns
+     * according to the map provided.
+     *
+     * The same map is used to return data with the correct names,
+     * now we make sure that the names are converted back to their
+     * database equivalents...
+     *
+     * @param String $model name of model
+     * @param Array $data data to rename columns for
+     *
+     * @return array
+     */
+    protected function mapColumnsForInput($model, $data)
     {
-        return $this->dataCommands;
+        $columnsForModel = array_flip($this->dataMap[$model]);
+        $return = array();
+        foreach ($data as $key => $value) {
+            $return[$columnsForModel[$key]] = $value;
+        }
+        return $return;
     }
 
     protected function formatDataForResponse($dataToFormat)
     {
         $return = array();
         foreach ($dataToFormat as $model => $realData) {
-            $return[$model] = array();
-            if (is_a($realData,'PropelModelPager')){
+            $return[$model]          = array();
+            $return['links'][$model] = 'http://localhost/api/' . $model . '/{' . $model . '.id}';
+            if (is_a($realData, 'PropelModelPager')) {
                 $tempData = array();
-                foreach($realData as $post){
+                foreach ($realData as $post) {
                     /** @noinspection PhpUndefinedMethodInspection */
                     $tempData[] = $post->toArray();
                 }
@@ -135,21 +157,26 @@ class API
                 $tempData = $realData->toArray();
             }
             foreach ($tempData as $post) {
-                $data = array();
+                $data       = array();
                 $primaryKey = null;
-                if(!is_array($post)){
+                if (!is_array($post)) {
                     /** @noinspection PhpUndefinedMethodInspection */
                     $primaryKey = $post->getPrimaryKey();
-                    $post = $post->toArray();
+                    $post       = $post->toArray();
                 } else {
                     $primaryKey = $post['Id'];
                 }
                 foreach ($this->dataMap[$model] as $key => $value) {
                     $data[$value] = is_numeric($key) ? $post[$value] : $post[$key];
                 }
-                $return[$model][$primaryKey] = (object)$data;
+                $return[$model][$primaryKey] = (object) $data;
             }
         }
         return $return;
+    }
+
+    public function returnDataCommands()
+    {
+        return $this->dataCommands;
     }
 }
